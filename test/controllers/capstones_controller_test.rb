3ba1a3f9845faeb62fcd794579237948a33fc3db
Derek McGee <dmcgee2001@gmail.com>
2023-12-18 16:36:11 -0500

    adds tests for capstone controller and sessions controller

--- test/controllers/capstones_controller_test.rb
+++ test/controllers/capstones_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class CapstonesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @student = Student.create(first_name: "Test", email: "test@test.com", password: "password")
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+  test "index" do
+    get "/capstones.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Capstone.count, data.length
+  end
 end
