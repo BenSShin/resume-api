147d0d31bb3d25895ebc43549d0c6b465ba3f1b8
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-18 13:03:03 -0600

    added show action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,4 +3,9 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
     render :index
   end
+
+  def show
+    @experience = Experience.find_by(id: params[:id])
+    render :show
+  end
 end
