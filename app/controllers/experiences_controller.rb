04250f8b8cc3a85fcd9c9697bd4777b1187fb46f
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-12-18 13:08:24 -0600

    added create action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -8,4 +8,25 @@ class ExperiencesController < ApplicationController
     @experience = Experience.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    start_date = Date.parse(params[:start_date], "%Y/%m/%d")
+    end_date = Date.parse(params[:end_date], "%Y/%m/%d")
+    total_days = (end_date - start_date).to_i
+
+    @experience = Experience.new({
+      start_date: start_date,
+      end_date: end_date,
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+      student_id: student_id[:student_id],
+    })
+    if @experience.valid?
+      @experience.save
+      render :show
+    else
+      render json: { errors: @experience.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
