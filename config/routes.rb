11f2175fdd3be57a8243d6decfc0753954f31603
Baila Simcha <bailasimcha@gmail.com>
2023-12-18 12:56:20 -0600

    add create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/educations" => "educations#index"
+  post "/educations" => "educations#create"
 end
--- test/controllers/educations_controller_test.rb
+++ test/controllers/educations_controller_test.rb
@@ -8,4 +8,11 @@ class EducationsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Education.count, data.length
   end
+
+  # test "create" do
+  #   assert_difference "Education.count", 1 do
+  #     post "/educations.json", params: { name: "lake", width: 800, height: 600 }
+  #     assert_response 200
+  #   end
+  # end
 end
