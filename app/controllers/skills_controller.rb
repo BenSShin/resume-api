3b17e755baa981398d408c5d980480f80fa93ec4
Derek McGee <dmcgee2001@gmail.com>
2023-12-18 13:59:20 -0500

    crud actions for skill controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,43 @@
 class SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+    render :index
+  end
+
+  def show
+    @skill = Skill.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @skill = Skill.new(
+      skill_name: params["skill_name"],
+      student_id: current_user.id,
+    )
+    @skill.save
+    if @skill.valid?
+      render :show
+    else
+      render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @skill = Skill.find_by(id: params["id"])
+    @skill.update(
+      skill_name: params["skill_name"] || @skill.skill_name,
+      student_id: @skill.student_id,
+    )
+    if @skill.valid?
+      render :show
+    else
+      render json: { errors: @skill.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    skill = Skill.find_by(id: params["id"])
+    skill.delete
+    render json: { message: "Skill successfully deleted" }
+  end
 end
