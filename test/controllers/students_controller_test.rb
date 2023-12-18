774640cfd7602098a894fe4fe0300eea416c5886
Jack <jconnolly444@gmail.com>
2023-12-18 16:31:09 -0600

    all student CRUD tests working

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -20,15 +20,29 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
 
     data = JSON.parse(response.body)
-    assert_equal ["first_name", "last_name", "email", "phone_number", "short_bio", "capstones", "educations", "experiences", "skills", "linkedin_url", "twitter_handle", "personal_url", "resume_url", "github_url", "photo"], data.keys
+    assert_equal ["id", "first_name", "last_name", "email", "phone_number", "short_bio", "capstones", "educations", "experiences", "skills", "linkedin_url", "twitter_handle", "personal_url", "resume_url", "github_url", "photo"], data.keys
+  end
+
+  setup do
+    @student = Student.create(first_name: "test", last_name: "test", email: "test@test.com", phone_number: "555-222-1234", short_bio: "test", linkedin_url: "testlinkedin.url", twitter_handle: "@testtwitter", personal_url: "test.url", resume_url: "stevenresume.url", github_url: "testgit.url", photo: "https://freerangestock.com/sample/2230/childs-drawing-of-happy-face.jpg", password: "password1")
+    post "/sessions.json", params: { email: "test@test.com", password: "password1" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
   end
 
   test "update" do
     student = Student.first
-    patch "/students/#{student.id}.json", params: { first_name: "Updated name" }
+    patch "/students/#{student.id}.json", params: { first_name: "Updated first_name" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Updated first_name", data["first_name"]
+  end
+
+  test "destroy" do
+    assert_difference "Student.count", -1 do
+      delete "/students/#{Student.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
+      assert_response 200
+    end
   end
 end
