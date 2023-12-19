d7ce4ee414523c74278e7dd9081796cd218daa48
Jack <jconnolly444@gmail.com>
2023-12-19 11:31:00 -0600

    fixed student controller test that was broken in merge

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,19 +1,19 @@
-# require "test_helper"
+require "test_helper"
 
-# class StudentsControllerTest < ActionDispatch::IntegrationTest
-#   test "index" do
-#     get "/students.json"
-#     assert_response 200
-
-#     data = JSON.parse(response.body)
-#     assert_equal Student.count, data.length
-#   end
-test "create" do
-  assert_difference "Student.count", 1 do
-    post "/students.json", params: { first_name: "test", last_name: "test l", email: "test@mail.com", phone_number: "test number", short_bio: "test bio", linkedin_url: "test url", twitter_handle: "test", personal_url: "test", resume_url: "test", github_url: "test", photo: "test", password: "test" }
+class StudentsControllerTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get "/students.json"
     assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Student.count, data.length
   end
-end
+  test "create" do
+    assert_difference "Student.count", 1 do
+      post "/students.json", params: { first_name: "test", last_name: "test l", email: "test@mail.com", phone_number: "test number", short_bio: "test bio", linkedin_url: "test url", twitter_handle: "test", personal_url: "test", resume_url: "test", github_url: "test", photo: "test", password: "test" }
+      assert_response 200
+    end
+  end
 
 test "show" do
   get "/students/#{Student.first.id}.json"
@@ -45,4 +45,4 @@ test "destroy" do
     assert_response 200
   end
 end
-# end
+end
